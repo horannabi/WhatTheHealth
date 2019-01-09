d9ff458ffb5dd2eef94fbdfcb5ebbccb055864f4
Bit <Bit@DESKTOP-QMURUQ1>
2019-01-09 21:38:17 +0900

    SeongMin: Ex/Diet Schedule,Claim,HashTag Service/Dao&Impl 생성

--- WhatTheHealth/src/main/java/com/wthealth/service/exschedule/ExScheduleService.java
+++ WhatTheHealth/src/main/java/com/wthealth/service/exschedule/ExScheduleService.java
@@ -1,5 +1,17 @@
 package com.wthealth.service.exschedule;
 
 public interface ExScheduleService {
+	
+	public void addExSchedule() throws Exception;
+	
+	public void getSchedule() throws Exception;
+	
+	public void updateExSchedule() throws Exception; 
+	
+	public void listExSchedule() throws Exception;
+	
+	public void getExHistoryChart() throws Exception;
+	
+	public void deleteExSchedule() throws Exception; 
 
 }
--- WhatTheHealth/src/main/java/com/wthealth/service/hashtag/HashTagDao.java
+++ WhatTheHealth/src/main/java/com/wthealth/service/hashtag/HashTagDao.java
@@ -2,4 +2,10 @@ package com.wthealth.service.hashtag;
 
 public interface HashTagDao {
 
+	public void addHashTag() throws Exception;
+	
+	public void getHashTag() throws Exception;
+	
+	public void updateHashTag() throws Exception; 
+	
 }
