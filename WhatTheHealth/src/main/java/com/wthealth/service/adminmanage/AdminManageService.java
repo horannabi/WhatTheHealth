b5e295c614197a86616783886097805504e341cc
bit <bit@DESKTOP-40MAN84>
2019-01-10 21:41:50 +0900

    donghwan : adminmanage, exinfo Mapper

--- WhatTheHealth/src/main/java/com/wthealth/service/adminmanage/AdminManageService.java
+++ WhatTheHealth/src/main/java/com/wthealth/service/adminmanage/AdminManageService.java
@@ -1,16 +1,19 @@
 package com.wthealth.service.adminmanage;
 
+import java.util.List;
 import java.util.Map;
 
 import com.wthealth.common.Search;
+import com.wthealth.domain.Point;
 import com.wthealth.domain.User;
 
 public interface AdminManageService {
 
-	public Map<String, Object> listUserAdminManage(Search search) throws Exception;
+	public List<User> listUserAdminManage(Search search) throws Exception;
 	
 	public User getAdminManage(String userId) throws Exception;
 	
 	public void updateUserAdminManage(User user) throws Exception;
+	
 	
 }
